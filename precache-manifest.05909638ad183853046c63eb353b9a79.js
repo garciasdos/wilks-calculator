18275db496f6c2d713ded1b500585629cd924df1
Diego Garcia <garciasdos@gmail.com>
2020-03-30 20:10:35 +0200

    Updates

---
+++ precache-manifest.05909638ad183853046c63eb353b9a79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e2d9cf9ed7ab96e97d277f38aa6c043",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eee557d12fedc27acad7",
+    "url": "/static/css/main.0c843ca8.chunk.css"
+  },
+  {
+    "revision": "8c1db625ee322db1d26b",
+    "url": "/static/js/2.984929d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.984929d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eee557d12fedc27acad7",
+    "url": "/static/js/main.a7a4a6de.chunk.js"
+  },
+  {
+    "revision": "04727cb18338fa55a4a3",
+    "url": "/static/js/runtime-main.911d1f49.js"
+  },
+  {
+    "revision": "7df7095b689ccedf52f3302238d8c15f",
+    "url": "/static/media/powerlifting_navarra-black.7df7095b.svg"
+  }
+]);
